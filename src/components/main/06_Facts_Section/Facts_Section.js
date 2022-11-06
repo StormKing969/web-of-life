52ab1ba663036544a573a08ee4c21e9fe1ba649f
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-11-05 20:24:39 -0500

    Facts Section Bug Fix

--- src/components/main/06_Facts_Section/Facts_Section.js
+++ src/components/main/06_Facts_Section/Facts_Section.js
@@ -9,9 +9,9 @@ import "./Facts_Section.scss";
 const Facts_Section = () => {
   return (
     <div className="facts_container">
-      <div className="container_info">
-        <h1 className="container_title">Fun Facts About The Environment</h1>
-        <p className="container_subtitle">
+      <div className="environmental_container_info">
+        <h1 className="environmental_container_title">Fun Facts About The Environment</h1>
+        <p className="environmental_container_subtitle">
           This world is a wonderful place with numerous mysteries within it.
           Though our environment has self-healing power, it is not a black hole
           that will not ever react to the harms caused by its population.
@@ -20,49 +20,49 @@ const Facts_Section = () => {
 
       <div className="environmental_cards_container">
         <div className="environmental_card_01">
-          <div className="card_info color_grey">
-            <p className="content">{Data[0].Fact}</p>
+          <div className="environmental_card_info environmental_color_grey">
+            <p className="environmental_content">{Data[0].Fact}</p>
           </div>
-          <div className="card_img">
+          <div className="environmental_card_img">
             <img src={Pollution} alt="pollution" />
           </div>
-          <div className="card_img_bg">
+          <div className="environmental_card_img_bg">
             <img src={Pollution} alt="pollution" />
           </div>
         </div>
 
         <div className="environmental_card_02">
-          <div className="card_info color_green">
-            <p className="content">{Data[1].Fact}</p>
+          <div className="environmental_card_info environmental_color_green">
+            <p className="environmental_content">{Data[1].Fact}</p>
           </div>
-          <div className="card_img">
+          <div className="environmental_card_img">
             <img src={Fungi} alt="fungi" />
           </div>
-          <div className="card_img_bg">
+          <div className="environmental_card_img_bg">
             <img src={Fungi} alt="fungi" />
           </div>
         </div>
 
         <div className="environmental_card_03">
-          <div className="card_info color_brown">
-            <p className="content">{Data[2].Fact}</p>
+          <div className="environmental_card_info environmental_color_brown">
+            <p className="environmental_content">{Data[2].Fact}</p>
           </div>
-          <div className="card_img">
+          <div className="environmental_card_img">
             <img src={Wood} alt="wood" />
           </div>
-          <div className="card_img_bg">
+          <div className="environmental_card_img_bg">
             <img src={Wood} alt="wood" />
           </div>
         </div>
 
         <div className="environmental_card_04">
-          <div className="card_info color_blue">
-            <p className="content">{Data[3].Fact}</p>
+          <div className="environmental_card_info environmental_color_blue">
+            <p className="environmental_content">{Data[3].Fact}</p>
           </div>
-          <div className="card_img">
+          <div className="environmental_card_img">
             <img src={Oil} alt="oil" />
           </div>
-          <div className="card_img_bg">
+          <div className="environmental_card_img_bg">
             <img src={Oil} alt="oil" />
           </div>
         </div>
