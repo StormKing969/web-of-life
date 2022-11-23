9aed8d98011535951f30597ad2971ef6e88071e6
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2022-11-23 01:17:37 -0600

    performance improvement

--- src/components/core/App.js
+++ src/components/core/App.js
@@ -1,25 +1,29 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Main from "../main/Main";
-import Contact from "../pages/Contact/Contact";
-import Course from "../pages/Course/Course";
-import Error from "../pages/Error/Error";
-import Info from "../pages/Info/Info";
-import Mission from "../pages/Mission/Mission";
 
-import "./App.scss"
+import "./App.scss";
+
+const Main = lazy(() => import("../main/Main"));
+const Contact = lazy(() => import("../pages/Contact/Contact"));
+const Course = lazy(() => import("../pages/Course/Course"));
+const Info = lazy(() => import("../pages/Info/Info"));
+const Mission = lazy(() => import("../pages/Mission/Mission"));
+const Error = lazy(() => import("../pages/Error/Error"));
 
 function App() {
   return (
     <Router>
       <div>
-        <Routes>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
             <Route exact path="/" element={<Main />} />
             <Route exact path="/info" element={<Info />} />
             <Route exact path="/courses" element={<Course />} />
             <Route exact path="/mission" element={<Mission />} />
             <Route exact path="/contact" element={<Contact />} />
             <Route exact path="/*" element={<Error />} />
-        </Routes>
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
